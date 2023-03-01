e8e83888b67fa98a5619fcab2208e5a9a44ebdd6
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-03-01 14:55:39 -0500

    stylisation de la page d'erreur et elimination de la page detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ProduitComponent } from './produit/produit.component';
-import { DetailComponent } from './detail/detail.component';
 import { EnteteComponent } from './entete/entete.component';
 import { NonTrouveeComponent } from './non-trouvee/non-trouvee.component';
 import { ListeComponent } from './liste/liste.component';
@@ -32,7 +30,6 @@ import { MatDialogModule } from '@angular/material/dialog';
     AppComponent,
     AccueilComponent,
     ProduitComponent,
-    DetailComponent,
     EnteteComponent,
     NonTrouveeComponent,
     ListeComponent,
